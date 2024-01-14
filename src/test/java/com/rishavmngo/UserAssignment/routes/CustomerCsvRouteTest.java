a1ff8cbb5d5a4203e77fd6eebc12bbac1e3aa7bc
rishavmngo <rishavinmngo@gmail.com>
2024-01-14 13:32:15 +0530

    rename unit test name for more clarity

--- src/test/java/com/rishavmngo/UserAssignment/routes/CustomerCsvRouteTest.java
+++ src/test/java/com/rishavmngo/UserAssignment/routes/CustomerCsvRouteTest.java
@@ -1,37 +1,25 @@
 package com.rishavmngo.UserAssignment.routes;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Configuration;
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
-import org.apache.camel.builder.AdviceWith;
-import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.MockEndpoints;
-import org.apache.camel.test.spring.junit5.MockEndpointsAndSkip;
-import org.apache.camel.test.spring.junit5.UseAdviceWith;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -88,6 +76,47 @@ public class CustomerCsvRouteTest {
 
 	}
 
+	@Test
+	void testCsvFileProcessToCallsAddCustomerToThrowException() throws Exception {
+
+		doThrow(new BadRequestException("Email already exists")).when(customerService)
+				.addCustomer(any(CustomerEntity.class));
+
+		producerTemplate.send("direct:csvFileProcessor", exchange -> {
+			exchange.getIn().setBody(CustomersUtils.createTestCustomerEntityA());
+			exchange.getIn().setHeader("CamelFileName", "test.csv");
+		});
+
+		verify(customerService).addCustomer(any(CustomerEntity.class));
+	}
+
+	@Test
+	void testCsvFileProcessorIfCallsAddCustomer() throws Exception {
+
+		doNothing().when(customerService).addCustomer(any());
+
+		producerTemplate.send("direct:csvFileProcessor", exchange -> {
+			exchange.getIn().setBody(CustomersUtils.createTestCustomerEntityA());
+			exchange.getIn().setHeader("CamelFileName", "test.csv");
+		});
+
+		verify(customerService).addCustomer(any(CustomerEntity.class));
+	}
+
+	@Test
+	void testingFileRoute() throws Exception {
+
+		doNothing().when(customerService).addCustomer(any());
+
+		producerTemplate.send("file:data/inbox", exchange -> {
+			exchange.getIn().setBody(new File("data/input/test.csv"));
+			exchange.getIn().setHeader("CamelFileName", "test.csv");
+		});
+
+		Thread.sleep(1000L);
+		assertFileMovedTo("data/inbox/.done/test.csv");
+	}
+
 	void test() throws Exception {
 		List<CustomerEntity> customers = Arrays.asList(
 				CustomersUtils.createTestCustomerEntityA(),
@@ -111,46 +140,6 @@ public class CustomerCsvRouteTest {
 
 		// CustomerEntity capturedCustomer = customerArgumentCaptor.getValue();
 		// assertThat(capturedCustomer).isEqualTo(customers.get(0));
-	}
-
-	@Test
-	void testOne() throws Exception {
-
-		doNothing().when(customerService).addCustomer(any());
-
-		producerTemplate.send("file:data/inbox", exchange -> {
-			exchange.getIn().setBody(new File("data/input/test.csv"));
-			exchange.getIn().setHeader("CamelFileName", "test.csv");
-		});
-
-		Thread.sleep(1000L);
-		assertFileMovedTo("data/inbox/.done/test.csv");
-	}
-
-	@Test
-	void testAnother() throws Exception {
-
-		doThrow(new BadRequestException("hello")).when(customerService).addCustomer(any(CustomerEntity.class));
-
-		producerTemplate.send("direct:csvFileProcessor", exchange -> {
-			exchange.getIn().setBody(CustomersUtils.createTestCustomerEntityA());
-			exchange.getIn().setHeader("CamelFileName", "test.csv");
-		});
-
-		verify(customerService).addCustomer(any(CustomerEntity.class));
-	}
-
-	@Test
-	void testAnotherOne() throws Exception {
-
-		doNothing().when(customerService).addCustomer(any());
-
-		producerTemplate.send("direct:csvFileProcessor", exchange -> {
-			exchange.getIn().setBody(CustomersUtils.createTestCustomerEntityA());
-			exchange.getIn().setHeader("CamelFileName", "test.csv");
-		});
-
-		verify(customerService).addCustomer(any(CustomerEntity.class));
 	}
 
 	private void assertFileMovedTo(String expectedPath) {
